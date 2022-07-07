3bbf4985b81d1aed3371b0dc12d5ff1599b8c2e0
kadun1 <kadunjj@gmail.com>
2022-07-08 00:18:59 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    /**
+     * 자바 기초를 올리기 위한 기본적인 학습 코딩 
+     * 몇몇 책을 기반으로 하여 코딩 스타일을 따라 습득할 예정
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
