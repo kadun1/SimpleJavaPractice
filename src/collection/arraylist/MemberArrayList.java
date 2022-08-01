47e71b26820221384900153e504b15b737a1b301
kadun1 <kadunjj@gmail.com>
2022-08-02 00:03:23 +0900

    java basic treeset & hashset

--- src/collection/arraylist/MemberArrayList.java
+++ src/collection/arraylist/MemberArrayList.java
@@ -3,6 +3,7 @@ package collection.arraylist;
 import collection.Member;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MemberArrayList {
     private ArrayList<Member> arrayList;
@@ -19,15 +20,30 @@ public class MemberArrayList {
         arrayList.add(index, member);
     }
 
+//    public boolean removeMember(int memberId) {
+//        for (int i = 0; i < arrayList.size(); i++) {
+//            Member member = arrayList.get(i);
+//            int tempId = member.getMemberId();
+//            if (tempId == memberId) {
+//                arrayList.remove(i);
+//                return true;
+//            }
+//        }
+//        System.out.println(memberId + "가 존재하지 않습니다.");
+//        return false;
+//    }
+
     public boolean removeMember(int memberId) {
-        for (int i = 0; i < arrayList.size(); i++) {
-            Member member = arrayList.get(i);
+        Iterator<Member> ir = arrayList.iterator();
+        while (ir.hasNext()) {
+            Member member = ir.next();
             int tempId = member.getMemberId();
             if (tempId == memberId) {
-                arrayList.remove(i);
+                arrayList.remove(member);
                 return true;
             }
         }
+        //끝날 때까지 삭제하려는 값을 찾지 못한 경우
         System.out.println(memberId + "가 존재하지 않습니다.");
         return false;
     }
